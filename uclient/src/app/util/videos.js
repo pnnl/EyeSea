feab8b082da73687a79161f74622babc7eade07d
Arthur McBain <arthur.mcbain@pnnl.gov>
2018-06-28 14:06:00 -0700

    Cleanup and hack removal of placebear, etc.

--- uclient/src/app/util/videos.js
+++ uclient/src/app/util/videos.js
@@ -4,7 +4,8 @@ export const formatDuration = function(video) {
 		video.analyses.find(analysis => analysis.status === 'FINISHED');
 
 	if (video.duration || analysis) {
-		let length = video.duration ||
+		let length =
+			video.duration ||
 			analysis.results[analysis.results.length - 1].frameIndex / video.fps;
 		let hours = Math.floor(length / 3600);
 		let minutes = Math.floor((length - hours * 3600) / 60);
