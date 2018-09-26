c3f4cf20db15c1167739c2cdd9333ec85614fcc9
Arthur McBain <arthur.mcbain@pnnl.gov>
2018-09-26 12:50:48 -0700

    Move out the graphs from their previous Videos component parent into a shared directory as they will be used by the Summary component. Also due to incidental commit mashing, this fixes the missing dep on whatwg-fetch and updates the package.json metadata.

--- uclient/src/app/Videos/BarGraph/index.js
+++
@@ -1,3 +0,0 @@
-export * from './BarGraph';
-import BarGraph from './BarGraph';
-export default BarGraph;
--- uclient/src/app/shared/StackedOccurrencesGraph/StackedOccurrencesGraph.js
+++ uclient/src/app/shared/StackedOccurrencesGraph/StackedOccurrencesGraph.js
@@ -19,7 +19,7 @@ export class StackedOccurrencesGraph extends React.PureComponent {
 	render() {
 		if (this.props.values) {
 			let box = this.svg && this.svg.getBoundingClientRect();
-			let maxX = ((box && box.width) || 100) - 16;
+			let maxX = ((box && box.width) || 100) - 25;
 			let maxY = (box && box.height) || 100;
 
 			let length = this.props.values.reduce(
@@ -125,9 +125,7 @@ export class StackedOccurrencesGraph extends React.PureComponent {
 
 			frames = Object.keys(frames)
 				.reverse()
-				.map(id => (
-					<path key={id} d={frames[id]} fill={colors[id]} />
-				));
+				.map(id => <path key={id} d={frames[id]} fill={colors[id]} />);
 
 			return (
 				<svg
