18e7491b0ee09d5f9101422777877eace5640da4
Andrew Avila <Andrew.Avila@pnnl.gov>
2018-12-11 13:54:49 -0800

    Fix summary percent time

--- server/eyesea_server.py
+++ server/eyesea_server.py
@@ -495,7 +495,7 @@ def video_statistics(vid):
     print(avg_bounding_box, total_detections)
     return fr()({
         'totalDetections': total_detections,
-        'percentTimeWithDetections': frames_with_detections / frame_count if frame_count else 0,
+        'percentTimeWithDetections': (frames_with_detections / frame_count) / len(a) if frame_count else 0,
         'minBoundingBoxArea': min_bounding_box if not np.isposinf(min_bounding_box) else 0,
         'avgBoundingBoxArea': avg_bounding_box / total_detections if total_detections else 0,
         'maxBoundingBoxArea': max_bounding_box if max_bounding_box >= 0 else 0,
