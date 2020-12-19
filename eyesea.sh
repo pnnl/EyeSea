da498a01b9e916b5a3b025a138d797e760374cc8
Shari Matzner <shari.matzner@pnnl.gov>
2020-12-19 11:07:26 -0800

    Added log files to eyesea.sh to capture error messages

--- eyesea.sh
+++ eyesea.sh
@@ -7,8 +7,8 @@ source $CONDA_BASE/etc/profile.d/conda.sh
 
 cd $EYESEA_ROOT/server
 conda activate eyesea-server
-nohup python eyesea_server.py >/dev/null 2>$HOME/eyesea.log &
+nohup python eyesea_server.py >/dev/null 2>$HOME/eyesea.server.log &
 
 cd $EYESEA_ROOT/uclient
 conda activate eyesea-client
-nohup yarn start >/dev/null 2>&1 &
+nohup yarn start >/dev/null 2>$HOME/eyesea.client.log &
